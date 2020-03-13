2f37af4ce301509e7fb97533935ff72f3d4ec6a2
jrimyak <imyakjake@gmail.com>
2020-03-13 08:00:46 -0500

    first

---
+++ src/components/Resume.js
@@ -0,0 +1,109 @@
+import React, { Component } from 'react';
+export default  class Resume extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="resume">
+
+         <div className="row education">
+
+            <div className="three columns header-col">
+               <h1><span>Education</span></h1>
+            </div>
+
+            <div className="nine columns main-col">
+              {
+                resumeData.education && resumeData.education.map((item)=>{
+                  return(
+                    <div className="row item">
+                       <div className="twelve columns">
+                          <h3>{item.UniversityName}</h3>
+                          <p className="info">
+                          {item.specialization}
+                          <span>&bull;</span> <em className="date">{item.MonthOfPassing} {item.YearOfPassing}</em></p>
+                          <p>
+                          {item.GPA}
+                          </p>
+                    
+                          <p>
+                          {item.Coursework}
+                          </p>
+                          <p>
+                          {item.Awards}
+                          </p>
+                          <p>
+                          {item.Activities}
+                          </p>
+                       </div>
+                    </div>
+                  )
+                })
+              }
+            </div>
+         </div>
+        <div className="row work">
+            <div className="three columns header-col">
+               <h1><span>Work</span></h1>
+            </div>
+
+            <div className="nine columns main-col">
+              {
+                resumeData.work && resumeData.work.map((item) => {
+                  return(
+                    <div className="row item">
+                       <div className="twelve columns">
+                          <h3>{item.CompanyName}</h3>
+                          <p className="info">
+                          {item.specialization}
+                          <span>&bull;</span> <em className="date">{item.MonthOfLeaving} {item.YearOfLeaving}</em></p>
+                          <p>
+                          </p>
+                          <p>
+                          {item.Bullet2}
+                          </p>
+                          <p>
+                         {item.Bullet3}
+                          </p>
+                          <p>
+                          {item.Bullet4}
+                          </p>
+                          <p>
+                          {item.Bullet5}
+                          </p>
+                       </div>
+
+                    </div>
+
+                  )
+                })
+              }
+            </div> 
+         </div>
+
+
+         <div className="row skill">
+
+            <div className="three columns header-col">
+               <h1><span>Skills</span></h1>
+            </div>
+
+            <div className="nine columns main-col">
+
+               <p>
+               {resumeData.skillsDescription1}
+               </p>
+               <p>
+               {resumeData.skillsDescription2}
+               </p>
+
+     
+   			
+
+   			</div>
+
+         </div>
+
+      </section>
+    );
+  }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,122 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Jake Imyak",
+    "role": "Full Stack Developer",
+    "linkedinId":"Phone: 847-912-1914 - Email: imyakjake@gmail.com",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I loves to create, build, and lead for any type of project. My curiosity and positivity allows me to learn and try a multiude of different things related to computer sciece.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/jacob-imyak/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/jrimyak",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://www.instagram.com/jakeimyak/",
+          "className":"fa fa-instagram"
+        }
+      ],
+    "aboutme":"I am a developer from the Chicagoland area. I am a sophomore studying Computer Science and Engineering student at Ohio State University. I co-founded a started called UpGrade, which is an app for peer-to-peer tutoring, and I worked as a full-stack developer on the team. I co-founded a start-up named Alright Development which provides software solutions for 11 local businesses. ",
+    "address":"58 E Fabish Drive",
+    "website":"https://rbhatia46.github.io",
+    "education":[
+      {
+        "UniversityName":"Ohio State University, College of Engineering",
+        "specialization":"Major: Computer Science and Engineering",
+        "MonthOfPassing":"Expected Graduation: May",
+        "YearOfPassing":"2022",
+        "GPA":"GPA: 3.66/4.00",
+        "Coursework":"Relevant Coursework: Data Structures and Algorithms, Discrete Math, Linear Algebra, Statistics for Engineers",
+        "Awards":"Awards: Trustees Scholarship, National Buckeye Scholar, Huskie Hack 1st Prize (2017), Hack Ridge 2nd Place and Best Overall Design (2018), Dean’s List (AU 18, SP 19, AU 19)",
+        "Activities":"Activities: D3 - Design, Develop, Deploy (Accelerator Team Leader), Coffee Roaster Club (President), Engineering House Learning Community, Undergraduate Student Government (Committee Representative), Cognitive Science Club (Member)"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"UpGrade",
+        "specialization":"Co-Founder/Full-Stack Developer",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2019",
+        "Bullet1":"Co-founded a startup that allows students to be connected academically by creating study groups, finding peer tutors, and allowing students to find fellow students in the same classes",
+        "Bullet2":"Built out a full-stack React Native application implementing Material Design on the frontend client-side",
+        "Bullet3":"Designed a server with NoSQL persistent storage and utilized CDN based image storage to improve efficiency",
+        "Bullet4":"Optimized client application using asynchronous API calls to improve rerender speed",
+        "Bullet5":"Set up a weekly cadence of agile-based development with establishing a positive, optimistic work environment"
+
+      },
+      {
+        "CompanyName":"Quad (Flip III LLC)",
+        "specialization":"Software Engineer",
+        "MonthOfLeaving":"May",
+        "YearOfLeaving":"2019",
+        "Bullet1":"Design and developed an end to end web-based React application that provides a unique experience for end-users visiting the site while also creating an admin dashboard to keep to data concurrent",
+        "Bullet2":"Integrated functionality for the admin dashboard allowing for CSV files to be parsed and saved to a database"
+      },
+      {
+        "CompanyName":"Alright Development",
+        "specialization":"Co-Founder/Software Engineer",
+        "MonthOfLeaving":"June",
+        "YearOfLeaving":"2015",
+        "Bullet1":"Consulted with 11 clients to produce client requirement spec sheets and integrated features to perfection",
+        "Bullet2":"Led communication with clients to ensure the solutions were implementing the feature set agreed upon",
+        "Bullet3":"Inherited client’s legacy codebase in HTML and Swift; debugged and improved the efficiency of the code"
+      }
+    ],
+    "skillsDescription1":"Languages: Java, Javascript, Python, HTML5, CSS3, MatLab, C, and Swift",
+    "skillsDescription2":"Tools: mongoDB, Firebase, AWS, Bootstrap, Material Design, React, Redux, Express, REST API and Git",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Mental Drive",
+        "description":"MentalDrive is an all-in-one app to help monitor someone with a mental disability. Mental Drive was awarded first overall prize at Northern Illinois Huskie Hack in 2017.",
+        "imgurl":"images/portfolio/mentaldrive.jpg",
+        "link":"https://devpost.com/software/mentaldrive"
+      },
+      {
+        "name":"Legos By Evan",
+        "description":"Legos By Evan was a freelance project that was for a small business in the area that sold Jewish lego products. The website was made using full HTML, CSS, and Javascript. It has helped grow the company and allow for people to connect with the owner.",
+        "imgurl":"images/portfolio/legos.jpg",
+        "link":"http://www.legosbyevan.com/"
+      },
+      {
+        "name":"ARBody",
+        "description":"2nd overall prize and best design winner at HackRidge 2018. Built out a native iOS app using Swift to curb obesity integrating augmented reality to preview meals. I integrated the NutritionIX API to track caloric intake and created interactive graphs to visualize progress",  
+        "imgurl":"images/portfolio/arbody.jpg",
+        "link":"https://www.hackerearth.com/challenges/hackathon/hack-ridge-20/dashboard/f7b3149/submission/"
+      },
+      {
+        "name":"ACTalented",
+        "description":"ACT Practice App published to the iOS App Store in January 2017. Contains practice tests, profile analytics, and tutorials. The app has over 6,000 downloads and is growing rapidly. The app has allowed for free tutoring that helps all students who need help but may not have the resources to learn how to take the test.",
+        "imgurl":"images/portfolio/act.jpg",
+        "link":"https://apps.apple.com/us/app/actalented-act-practice-tests/id1202591036"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
